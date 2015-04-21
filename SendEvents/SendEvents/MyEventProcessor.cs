e7bcd8f027f5f4126f8659a84264c6c4ceccb8ef
hanzzhang <hanz@microsoft.com>
2015-04-21 14:24:32 -0700

    add SendEvent project

---
+++ SendEvents/SendEvents/MyEventProcessor.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.ServiceBus.Messaging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SendEvents
+{
+    public class MyEventProcessor : IEventProcessor
+    {
+
+        public Task CloseAsync(PartitionContext context, CloseReason reason)
+        {
+            return Task.FromResult(true);
+        }
+
+        public Task OpenAsync(PartitionContext context)
+        {
+            return Task.FromResult(true);
+        }
+
+        public async Task ProcessEventsAsync(PartitionContext context, IEnumerable<EventData> messages)
+        {
+            foreach (var message in messages)
+            {
+                var eventType = message.Properties["event-type"];
+                var bytes = message.GetBytes();
+                if (eventType.ToString() == "utf8string")
+                {
+                    var body = System.Text.Encoding.UTF8.GetString(bytes);
+                    Console.WriteLine("got:" + body);
+                }
+                else
+                {
+                    // Record that you don't know what to do with this event
+                }
+            }
+            await context.CheckpointAsync();
+        }
+    }
+}
---
+++ SendEvents/SendEvents/Program.cs
@@ -0,0 +1,120 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved. See License.txt in the project root for license information.
+
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.ServiceBus.Messaging;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+using Microsoft.ServiceBus;
+
+namespace SendEvents
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SendEvents();
+
+            RegisterEventProcessor();
+
+            Console.WriteLine("Press any key to exit");
+            Console.Read();
+        }
+        static void RegisterEventProcessor()
+        {
+            string hostName = "MyEventProcessorHost";
+            string eventHubName = ConfigurationManager.AppSettings["EventHubName"];
+            string eventHubNamespace = ConfigurationManager.AppSettings["EventHubNamespace"];
+            string listenPolicyName = ConfigurationManager.AppSettings["ListenPolicyName"];
+            string listenPolicyKey = ConfigurationManager.AppSettings["ListenPolicyKey"];
+            string storageAccountName = ConfigurationManager.AppSettings["StorageAccountName"];
+            string storageAccountKey = ConfigurationManager.AppSettings["StorageAccountKey"];
+
+            string eventHubConnectionStringForListen = string.Format(
+                "Endpoint=sb://{0}.servicebus.windows.net/;SharedAccessKeyName={1};SharedAccessKey={2};TransportType=Amqp",
+                eventHubNamespace, listenPolicyName, listenPolicyKey);
+
+            string storageConnectionString = string.Format(
+                "DefaultEndpointsProtocol=http;AccountName={0};AccountKey={1};",
+                storageAccountName, storageAccountKey);
+
+            var host = new EventProcessorHost(hostName,
+                                              eventHubName,
+                                              EventHubConsumerGroup.DefaultGroupName,
+                                              eventHubConnectionStringForListen,
+                                              storageConnectionString
+                                              );
+            host.RegisterEventProcessorAsync<MyEventProcessor>().Wait();
+            Console.WriteLine("RegisterEventProcessor Completed");
+        }
+        static void SendEvents()
+        {
+            // Get values from configuration
+            int numberOfDevices = int.Parse(ConfigurationManager.AppSettings["NumberOfDevices"]);
+            string eventHubName = ConfigurationManager.AppSettings["EventHubName"];
+            string eventHubNamespace = ConfigurationManager.AppSettings["EventHubNamespace"];
+            string sendPolicyName = ConfigurationManager.AppSettings["SendPolicyName"];
+            string sendPolicyKey = ConfigurationManager.AppSettings["SendPolicyKey"];
+
+            string eventHubConnectionString = string.Format(
+                "Endpoint=sb://{0}.servicebus.windows.net/;SharedAccessKeyName={1};SharedAccessKey={2};TransportType=Amqp",
+                eventHubNamespace, sendPolicyName, sendPolicyKey);
+
+            var client = EventHubClient.CreateFromConnectionString(eventHubConnectionString, eventHubName);
+
+            // Configure JSON to serialize properties using camelCase
+            JsonConvert.DefaultSettings = () => new JsonSerializerSettings { ContractResolver = new CamelCasePropertyNamesContractResolver() };
+
+            var random = new Random();
+
+            try
+            {
+                Console.WriteLine("Sending messages to Event Hub {0}", client.Path);
+                //while (!Console.KeyAvailable)
+                {
+                    var tasks = new List<Task>();
+
+                    // One event per device
+                    for (int devices = 0; devices < numberOfDevices; devices++)
+                    {
+                        // Create the event
+                        var info = new Event()
+                        {
+                            Id = devices.ToString(),
+                            Lat = -30 + random.Next(75),
+                            Lng = -120 + random.Next(70),
+                            Time = DateTime.UtcNow.Ticks,
+                            Code = (310 + random.Next(20)).ToString()
+                        };
+
+                        // Serialize to JSON
+                        var serializedString = JsonConvert.SerializeObject(info);
+
+                        // Create the message data
+                        var bytes = Encoding.UTF8.GetBytes(serializedString);
+                        var eventData = new EventData(bytes)
+                        {
+                            //PartitionKey = info.Id
+                        };
+                        eventData.Properties.Add("event-type", "utf8string");
+
+                        // Send the message to Event Hub
+                        Console.WriteLine("send:" + serializedString);
+                        tasks.Add(client.SendAsync(eventData));
+                    }
+
+                    Task.WaitAll(tasks.ToArray());
+                }
+            }
+            catch (Exception exp)
+            {
+                Console.WriteLine("Error on send: " + exp.Message);
+            }
+            Console.WriteLine("SendEvents Completed");
+            Console.WriteLine();
+        }
+    }
+}
